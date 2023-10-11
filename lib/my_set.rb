dd812a77ef2fee6695c6c3f4627f2b1ae65e1265
Lucie Kayser-Bril <l.kayserbril@gmail.com>
2023-10-11 12:30:48 -0600

    commit tree 3a35cfc5daa6dac0be63d8bb1e8b50067f2ea061

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,1 +1,42 @@
-# your code here
+class MySet
+    def initialize(enumerable = [])
+        @hash = {}
+        enumerable.each do |value|
+            @hash[value] = true
+        end
+    end
+
+    def include?(value)
+        @hash.has_key?(value)
+    end
+
+    def add(value)
+        @hash[value] = true
+        self
+    end
+
+    def delete(value)
+        @hash.delete(value)
+        self
+    end
+
+    def size
+        @hash.size
+    end
+
+    def self.[](values)
+        new = [values]
+    end
+
+    def clear
+        @hash.clear
+        self
+    end
+
+    def each
+        @hash[value].each do |value|
+            puts |value|
+        end
+    end
+
+end
